6cc9ec2253d8f805ffedc95edd91a9bf042537d1
heatherhops2it <heathergallowaybda@gmail.com>
2025-09-25 13:51:36 -0400

    removed extraneous/test code and finalised to the mapply/lapply system with 3 rounds (batches of 1000)

--- scripts/data-exploration/indices_exploration.R
+++ scripts/data-exploration/indices_exploration.R
@@ -7,96 +7,82 @@
 library(tidyverse)
 library(seewave)   # install.packages("seewave")
 library(tuneR)  # install.packages("tuneR")
-library(soundecology)
 
 
 
 
-# test file ---------------------------------------------------------------
-
-aci_values <- data.frame(NULL)
-
-mgv_0518_2100 <- readWave("C:/Users/hng9/Downloads/output_tests/S1130_048K_MGV_10457639_20250518_210000.wav")
-
-interim_aci <- c("mgv_0518_2100", ACI(mgv_0518_2100, flim = c(0,2), wl = 2048, nbwindows = 10))
-
-aci_values <- rbind(aci_values, interim_aci)
-
-rm(mgv_0518_2100)
-
-
-
-# test loop ---------------------------------------------------------------
-## stopping at 41 files...
-aci_values <- data.frame(NULL)
-my_list <- NULL
-
-file_list <- list.files("C:/Users/hng9/Music/BDAKFP_2025_LVR/", full.names = TRUE)
-file_p1 <- file_list[1:50]
-
-
-for (i in file_list) {
-  soundwave <- readWave(i)
-  interim_aci <- acoustic_complexity(soundwave, max_freq = 2000, j = 10, fft_w = 2048)
-  
-  my_list <- interim_aci[c(1,3)] |>
-    as.data.frame()
-  my_list$date <- substr(i, 61, 75)
-  
-  aci_values <- rbind(aci_values, my_list)
-  
-  rm(soundwave)
-}
-
-
-
-
-# test mapply -------------------------------------------------------------
+# Get ACI -------------------------------------------------------------
 
 files <- list.files("C:/Users/hng9/Music/BDAKFP_2025_LVR/", full.names = TRUE)
 
+
+## first batch
 file_list <- mapply(readWave, 
                     files[1:1000], 
                     from = 0, 
                     to = 1, 
                     units = "minutes")
 
-aci_list <- lapply(file_list,
+aci_raw <- lapply(file_list,
                    ACI,
                    wl = 2048,
                    flim = c(0,2))
 
-aci_list <- as.data.frame(aci_list)
+aci_raw <- as.data.frame(aci_raw)
 
-testing <- t(aci_list) |> 
-  as.data.frame()
-testing <- testing |> mutate(date = substr(row.names(testing), 61, 75))
-
-
-
-
-###############################
-
-
-for (i in file_list) {
-  acoustic_complexity(i, max_freq = 2000, fft_w = 2048)
-}
+aci_fin <- t(aci_raw) |> 
+  as.data.frame() |> 
+  rename(aci_value = V1)
+aci_fin <- aci_fin |> mutate(date = substr(row.names(aci_fin), 61, 75))
 
 
 
 
+## second batch
+file_list <- mapply(readWave, 
+                    files[1001:2000], 
+                    from = 0, 
+                    to = 1, 
+                    units = "minutes")
 
-aci_list <- lapply(file_list,
-                   acoustic_complexity, 
-                     max_freq = 2000,
-                     fft_w = 2048)
+aci_raw <- lapply(file_list,
+                  ACI,
+                  wl = 2048,
+                  flim = c(0,2))
 
-aci_values <- aci_list[c(1,3)] |> 
-  as.data.frame()
+aci_raw <- as.data.frame(aci_raw)
+
+aci_proc <- t(aci_raw) |> 
+  as.data.frame() |> 
+  rename(aci_value = V1)
+aci_proc <- aci_proc |> mutate(date = substr(row.names(aci_proc), 61, 75))
+
+aci_fin <- aci_fin |> 
+  rbind(aci_proc)
 
 
 
 
+## third batch
+file_list <- mapply(readWave, 
+                    files[2001:length(files)], 
+                    from = 0, 
+                    to = 1, 
+                    units = "minutes")
 
-aci.low.values <- c(aci.low.start[1,])
+aci_raw <- lapply(file_list,
+                  ACI,
+                  wl = 2048,
+                  flim = c(0,2))
+
+aci_raw <- as.data.frame(aci_raw)
+
+aci_proc <- t(aci_raw) |> 
+  as.data.frame() |> 
+  rename(aci_value = V1)
+aci_proc <- aci_proc |> mutate(date = substr(row.names(aci_proc), 61, 75))
+
+aci_fin <- aci_fin |> 
+  rbind(aci_proc)
+
 
