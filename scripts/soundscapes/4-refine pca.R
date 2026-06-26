527440e6d957c20c7068edca32ad3a4ad378b650
heatherhops2it <heathergallowaybda@gmail.com>
2026-06-26 11:22:37 -0400

    pca and refinement on all sounds

--- scripts/soundscapes/4-refine pca.R
+++ scripts/soundscapes/4-refine pca.R
@@ -19,7 +19,9 @@ df <- bind_rows(lvr, mgv) |>
   clean_names() |> 
   select(-view, -channel, -begin_time_s, -end_time_s, 
          -low_freq_hz, -high_freq_hz, -begin_file, -begin_path,
-         -file_offset_s, -colour)
+         -file_offset_s, -colour) |> 
+  filter(site %in% c("LVR", "MGV")) |> 
+  filter(selection != "4262")
 
 ## only show the aquatic sounds
 fsh <- df |> 
@@ -30,16 +32,69 @@ fsh <- df |>
 
 
 
+# all sounds --------------------------------------------------------------
+
+## run the PCA
+pc_all <- df |> 
+  select(-selection, -source, -type, -site, -pulse_count) |> 
+  prcomp(center = TRUE,
+         scale. = TRUE)
+
+## make a plot
+autoplot(pc_all, data = df, colour = 'source', shape = 'site', size = 4,
+         loadings = TRUE, loadings.label = TRUE) +
+  labs(title = "all sounds")
+
+## the first two PCs explain 59.89% of the variation
+
+
+## check for correlation between variables
+cor_df <- df |> 
+  select(-selection, -source, -type, -site, -pulse_count) |> 
+  cor(method = c("pearson", "kendall", "spearman")) |> 
+  melt()
+cor_df |> 
+  mutate(rounded = round(value, digits = 2)) |> 
+  ggplot(aes(x = Var1, y = Var2, fill = value)) +
+  geom_tile() +
+  geom_text(aes(Var2, Var1, label = rounded)) +
+  theme(axis.text.x = element_text(angle = 45))
+# summarise the correlation values to see if there are some variables that typically have less correlation
+cor_df |> 
+  group_by(Var1) |> 
+  summarise(total = sum(value))
+
+
+
+## refine the dataframe to use variables with max eigenvalues and min correlation(?)
+df_ref <- df |> 
+  select(site, type, source,
+         center_time_rel, dur_50_percent_s, bw_50_percent_hz, freq_5_percent_hz)
+pc_df_ref <- df_ref |> 
+  select(-site, -type, -source) |> 
+  prcomp(center = TRUE,
+         scale. = TRUE)
+
+autoplot(pc_df_ref, data = df_ref, colour = 'source', shape = 'site', size = 4,
+         loadings = TRUE, loadings.label = TRUE) +
+  labs(title = "refined all sounds")
+
+## now the first two PCs explain XX% of the variation
+
+
+
+
+
 # all aquatic sounds ------------------------------------------------------
 
 ## run the PCA
-pc_all <- fsh |> 
+pc_aq <- fsh |> 
   select(-selection, -type, -site, -pulse_count) |> 
   prcomp(center = TRUE,
          scale. = TRUE)
 
 ## make a plot
-autoplot(pc_all, data = fsh, colour = 'site', 
+autoplot(pc_aq, data = fsh, colour = 'site', 
          loadings = TRUE, loadings.label = TRUE) +
   labs(title = "all aquatic sounds")
 
@@ -156,3 +211,8 @@ autoplot(pc_ham, data = fsh_ham, colour = 'site',
 ## the first two PCs explain 51.42% of the variation
 
 ## somewhat dffcult to move past this point while still comparing the two sites, since there are only two LVR harmonics
+
+
+
+
+
